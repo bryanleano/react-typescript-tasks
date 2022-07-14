4542a5af98df759c55789642ac6dd604b9d82a83
Bryan Leanoe <bryan.leano@gmail.com>
2022-07-13 20:31:18 -0600

    added todo logic

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './styles.css';
 
-const InputField = () => {
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>,
+  handleAdd: (e: React.FormEvent) => void
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
   return (
-    <form className='input'>
-      <input type='input' placeholder='Enter a task' className='input_box' />
+    <form className='input' onSubmit={handleAdd}>
+      <input value={todo} onChange={(e) => setTodo(e.target.value)} type='input' placeholder='Enter a task' className='input_box' />
       <button className='input_submit' type='submit'>Go</button>
     </form>
   )
